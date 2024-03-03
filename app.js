2d6240e13acdf63e4737c677af8d0cea6aba3827
Finn Moffett <finn.moffett@gmail.com>
2024-03-03 10:47:48 +0000

    Add data processing function and input box
    Added data processing function that converts the json into an array of
    desired data for displaying. Added an input box for inputting the
    location of desired weather forecast. Set up empty display function for
    adding date to the webpage. Will now add to the page, style, and add
    an appropirate gif and loading page.

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-async function weather(location) {
-  let source = await fetch(`https://api.weatherapi.com/v1/current.json?key=7d9c8c298c404131a76112436242602&q=${location}`, {mode: 'cors'})
+const loc = document.getElementById('location')
+const form = document.querySelector('form')
+
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  weather(loc.value)
+})
+
+async function weather(loc) {
+  let source = await fetch(`https://api.weatherapi.com/v1/current.json?key=7d9c8c298c404131a76112436242602&q=${loc}`, {mode: 'cors'})
   let response = await source.json()
-  console.log(response)
+  process(response)
 }
 
-weather('london')
+function process(response) {
+  let weatherData = []
+  console.log(response)
+  weatherData.push(response.current.temp_c)
+  weatherData.push(response.current.feelslike_c)
+  weatherData.push(response.current.condition.text)
+  weatherData.push(response.current.wind_mph)
+  display(weatherData)
+}
+
+function display(weatherData) {
+  console.log(weatherData)
+}
+
+
 
 
